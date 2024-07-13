69567423583b20c52a6c67524768c83d41620d68
GustavoFabri <gustavofabri760@gmail.com>
2024-07-13 15:44:27 -0300

    codigo ies

--- frontGerenciador/src/components/IESS.tsx
+++ frontGerenciador/src/components/IESS.tsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { Box, Select, Spinner } from '@chakra-ui/react';
 import axios from 'axios';
 
-
 // Definição do tipo IES
 type Ies = {
   codigo: string;
@@ -17,7 +16,11 @@ const listarTodasIes = async () => {
   return response.data;
 };
 
-const IESSelect = () => {
+interface IESSelectProps {
+  onSelectIes: (codigo: string) => void;
+}
+
+const IESSelect: React.FC<IESSelectProps> = ({ onSelectIes }) => {
   const [iesList, setIesList] = useState<Ies[]>([]);
   const [selectedIes, setSelectedIes] = useState<string>('');
   const [loading, setLoading] = useState<boolean>(true);
@@ -26,9 +29,12 @@ const IESSelect = () => {
     const fetchIes = async () => {
       try {
         const data = await listarTodasIes();
-
         console.log('Dados recebidos da API:', data);
-        setIesList(data);
+        if (Array.isArray(data)) {
+          setIesList(data);
+        } else {
+          console.error('Os dados recebidos não são um array:', data);
+        }
         setLoading(false);
       } catch (error) {
         console.error('Erro ao buscar IES:', error);
@@ -39,12 +45,18 @@ const IESSelect = () => {
     fetchIes();
   }, []);
 
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedCodigo = e.target.value;
+    setSelectedIes(selectedCodigo);
+    onSelectIes(selectedCodigo);
+  };
+
   return (
     <Box p={5}>
       {loading ? (
         <Spinner />
       ) : (
-        <Select placeholder="Selecione uma IES" value={selectedIes} onChange={(e) => setSelectedIes(e.target.value)}>
+        <Select placeholder="Selecione uma IES" value={selectedIes} onChange={handleChange}>
           {iesList.map((ies) => (
             <option key={ies.codigo} value={ies.codigo}>
               {ies.nome}
--- frontGerenciador/src/screens/turma/modal/TurmaForm.tsx
+++ frontGerenciador/src/screens/turma/modal/TurmaForm.tsx
@@ -1,123 +1,104 @@
-import { Box, Button, FormControl, FormLabel, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Spinner } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
-import { Turma } from "../../../models/Turma";
-import { alterarTurma, salvarTurma } from "../../../services/apiTurma";
-import IESSelect from "../../../components/IESS";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Spinner,
+} from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import { Turma } from '../../../models/Turma';
+import { alterarTurma, salvarTurma } from '../../../services/apiTurma';
+import IESSelect from '../../../components/IESS';
 
 interface TurmaFormProps {
-    turma: Turma | null;
-    onClose: () => void
+  turma: Turma | null;
+  onClose: () => void;
 }
 
-  // Função para listar todas as IES (simulação de uma chamada de API)
-  
+const TurmaForm: React.FC<TurmaFormProps> = ({ turma, onClose }) => {
+  const [formData, setFormData] = useState<Omit<Turma, 'codigo'>>({
+    nome: '',
+    dataCriacao: new Date(),
+    dataInicioPeriodo: new Date(),
+    dataFinalPeriodo: new Date(),
+    iesCodigo: '',
+  });
 
-const TurmaForm: React.FC<TurmaFormProps> = ({turma, onClose}) => {
-
-    const [formData, setFormData] = useState<Omit<Turma, 'codigo'>>({
-        nome : '',
-        dataCriacao: new Date(),
-        dataInicioPeriodo: new Date(),
-        dataFinalPeriodo: new Date(),
-        iesCodigo: '',
-        
-    })
-
-    
-
-    useEffect(() => {
-        if(turma){
-            setFormData({
-                nome : turma.nome,
-                dataCriacao: turma.dataCriacao,
-                dataInicioPeriodo: turma.dataInicioPeriodo,
-                dataFinalPeriodo: turma.dataFinalPeriodo,
-                iesCodigo: turma.iesCodigo,
-                
-            })
-        }
-    }, [turma])
-
-    
-
-    const handleChangeText = (ev: React.ChangeEvent<HTMLInputElement>) =>{
-
-        const {name, value} = ev.target;
-
-        setFormData({...formData, [name]: value})
+  useEffect(() => {
+    if (turma) {
+      setFormData({
+        nome: turma.nome,
+        dataCriacao: turma.dataCriacao,
+        dataInicioPeriodo: turma.dataInicioPeriodo,
+        dataFinalPeriodo: turma.dataFinalPeriodo,
+        iesCodigo: turma.iesCodigo,
+      });
     }
+  }, [turma]);
 
+  const handleChangeText = (ev: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = ev.target;
+    setFormData({ ...formData, [name]: value });
+  };
 
-    const handleSubmit = async (ev: React.FormEvent) => {
-    
-        ev.preventDefault()
+  const handleSelectIes = (codigo: string) => {
+    setFormData({ ...formData, iesCodigo: codigo });
+  };
 
-        try{
+  const handleSubmit = async (ev: React.FormEvent) => {
+    ev.preventDefault();
 
-            if(turma){
-                await alterarTurma(turma.codigo, formData)
-            }else{
-                await salvarTurma(formData)
-            }
-            onClose()
-            window.location.reload()
-
-        }catch(error){
-
-        }
-
+    try {
+      if (turma) {
+        await alterarTurma(turma.codigo, formData);
+      } else {
+        await salvarTurma(formData);
+      }
+      onClose();
+      window.location.reload();
+    } catch (error) {
+      console.error('Erro ao salvar a turma:', error);
     }
+  };
 
-
-    return (
-
-        <Modal isOpen={true} onClose={onClose}>
-
-            <ModalOverlay/>
-
-            <ModalContent>
-
-                <ModalHeader>{turma ? 'Alterar TURMA' : 'Cadastrar TURMA'}</ModalHeader>
-                
-                <ModalCloseButton/>
-
-                <form onSubmit={handleSubmit}>
-
-                    <ModalBody>
-
-                        <FormControl id="nome" mb={5}>
-                            <FormLabel>Nome da turma</FormLabel>
-                            <Input type="text" name="nome" value={formData.nome} onChange={handleChangeText} required/>
-                        </FormControl>
-
-                        <IESSelect>
-
-                        </IESSelect>
-           
-                    </ModalBody>
-
-                    <ModalFooter>
-
-                        <Button colorScheme="blue" mr={3} type="submit">
-                            {turma ? 'Alterar': 'Cadastrar'}
-                        </Button>
-
-                        <Button onClick={onClose}>Cancelar</Button>
-
-                    </ModalFooter>
-
-                </form>
-
-            </ModalContent>
-
-        </Modal>
-
-
-    )
-
-
-
-
-}
+  return (
+    <Modal isOpen={true} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>{turma ? 'Alterar TURMA' : 'Cadastrar TURMA'}</ModalHeader>
+        <ModalCloseButton />
+        <form onSubmit={handleSubmit}>
+          <ModalBody>
+            <FormControl id="nome" mb={5}>
+              <FormLabel>Nome da turma</FormLabel>
+              <Input
+                type="text"
+                name="nome"
+                value={formData.nome}
+                onChange={handleChangeText}
+                required
+              />
+            </FormControl>
+            <IESSelect onSelectIes={handleSelectIes} />
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} type="submit">
+              {turma ? 'Alterar' : 'Cadastrar'}
+            </Button>
+            <Button onClick={onClose}>Cancelar</Button>
+          </ModalFooter>
+        </form>
+      </ModalContent>
+    </Modal>
+  );
+};
 
 export default TurmaForm;
