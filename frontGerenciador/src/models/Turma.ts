2240f01c6e8a401f574bc9e526633bd9ab82f25b
GustavoFabri <gustavofabri760@gmail.com>
2024-07-13 14:23:26 -0300

    models

---
+++ frontGerenciador/src/models/Turma.ts
@@ -0,0 +1,14 @@
+import { Ies } from "./Ies";
+
+export interface Turma{
+
+    codigo: string;
+    nome: string;
+    dataCriacao: Date;
+    dataInicioPeriodo: Date;
+    dataFinalPeriodo: Date;
+    ies: Ies;
+    iesCodigo: string;
+    usuario: string;
+    
+}
